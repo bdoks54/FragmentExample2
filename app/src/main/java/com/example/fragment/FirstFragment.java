55a9e575b8229f650ab39041d946b3acde00e17b
bdoks54 <wnddhks1177@gmail.com>
2021-01-06 18:52:56 +0900

    .

---
+++ app/src/main/java/com/example/fragment/FirstFragment.java
@@ -0,0 +1,45 @@
+package com.example.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FirstFragment extends Fragment {
+    private OnButtonClickedListener mOnButtonClickedListener;
+
+    //인자가 없는 FirstFragment 생성자 필수
+    public FirstFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof OnButtonClickedListener){
+            mOnButtonClickedListener = (OnButtonClickedListener) context;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        Button helloButton = view.findViewById(R.id.hello_button);
+        helloButton.setOnClickListener(v -> mOnButtonClickedListener.onButtonClicked());
+        return view;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mOnButtonClickedListener = null;
+    }
+
+    public interface OnButtonClickedListener{
+        void onButtonClicked();
+    }
+}
